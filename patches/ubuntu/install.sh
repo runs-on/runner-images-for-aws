6b33a09e54ca9a2779710c4d355c29286944f868
Cyril Rohr <cyril.rohr@gmail.com>
2023-11-28 13:14:18 +0000

    fix

--- patches/ubuntu/install.sh
+++ patches/ubuntu/install.sh
@@ -1,11 +1,12 @@
 retry_command() {
     local retries=$1
     shift
+    echo "Retrying command for $retries times..."
     local count=0
     until "$@" || [[ $count -eq $retries ]]; do
-        echo "!!!!! Unable to execute command. Retrying in 5s..." >&2
+        echo "Unable to execute command. Retrying later..." >&2
         count=$((count+1))
-        sleep 5
+        sleep 10
     done
     return $?
 }
