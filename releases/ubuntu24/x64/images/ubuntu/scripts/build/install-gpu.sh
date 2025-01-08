befc39f0610a31144cfdd5e00a4f44157f540570
Cyril Rohr <hey@cyrilrohr.com>
2025-01-08 10:55:40 +0100

    Sync from upstream

--- releases/ubuntu24/x64/images/ubuntu/scripts/build/install-gpu.sh
+++ releases/ubuntu24/x64/images/ubuntu/scripts/build/install-gpu.sh
@@ -5,11 +5,26 @@ set -eo pipefail
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
+. /etc/os-release
+
+DIST_SLUG=""
+case $VERSION_CODENAME in
+    noble)
+        DIST_SLUG="ubuntu2404"
+        ;;
+    jammy)
+        DIST_SLUG="ubuntu2204"
+        ;;
+    *)
+        echo "Unsupported version codename: $VERSION_CODENAME"
+        exit 1
+        ;;
+esac
+
 # NVIDIA CUDA drivers
 DEBIAN_FILE="cuda-keyring_1.1-1_all.deb"
-REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/$DEBIAN_FILE"
+REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/$DIST_SLUG/x86_64/$DEBIAN_FILE"
 GPG_KEY="/usr/share/keyrings/cuda-archive-keyring.gpg"
-REPO_PATH="/etc/apt/sources.list.d/cuda-ubuntu2404-x86_64.list"
 
 wget $REPO_URL
 sudo dpkg -i $DEBIAN_FILE
@@ -26,7 +41,6 @@ fi
 
 rm $DEBIAN_FILE
 rm $GPG_KEY
-rm $REPO_PATH
 
 # NVIDIA container toolkit
 REPO_URL="https://nvidia.github.io/libnvidia-container/stable/deb/\$(ARCH)"
