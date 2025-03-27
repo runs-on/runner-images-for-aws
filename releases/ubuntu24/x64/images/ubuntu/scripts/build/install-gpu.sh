8b2e6bd93c856ca0f1a4d16bc1b325ed50a09585
Cyril Rohr <hey@cyrilrohr.com>
2025-03-27 13:07:27 +0100

    Sync

--- releases/ubuntu24/x64/images/ubuntu/scripts/build/install-gpu.sh
+++ releases/ubuntu24/x64/images/ubuntu/scripts/build/install-gpu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -eo pipefail
+
+HELPER_SCRIPTS=${HELPER_SCRIPTS:-"/imagegeneration/helpers"}
 
 source $HELPER_SCRIPTS/os.sh
 source $HELPER_SCRIPTS/etc-environment.sh
@@ -14,51 +15,46 @@ else
     exit 1
 fi
 
-# Ensure the root partition is resized
-cloud-init single --name growpart
-cloud-init single --name resizefs
+set -eox pipefail
 
-# NVIDIA CUDA drivers
+# Ensure the root partition is resized
+cloud-init single --name cc_growpart
+cloud-init single --name cc_resizefs
+
+# NVIDIA CUDA drivers and toolkit
 DEBIAN_FILE="cuda-keyring_1.1-1_all.deb"
 REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/$DIST_SLUG/x86_64/$DEBIAN_FILE"
-
 wget $REPO_URL
 dpkg -i $DEBIAN_FILE && rm $DEBIAN_FILE
-apt-get update -qq
-
-package="cuda-drivers"
-version="latest"
-if [[ $version == "latest" ]]; then
-    apt-get install --no-install-recommends "$package"
-else
-    version_string=$(apt-cache madison "$package" | awk '{ print $3 }' | grep "$version" | head -1)
-    apt-get install --no-install-recommends "${package}=${version_string}"
-fi
-
-apt install nvidia-cuda-toolkit cuda-toolkit -y
-
-# Add CUDA to PATH
-path=$(ls /usr/local/cuda-*/bin | head -1)
-prepend_etc_environment_path "$path"
-library_path=$(ls /usr/local/cuda-*/lib64 | head -1)
-prepend_etc_environment_variable "LD_LIBRARY_PATH", "$library_path"
 
 # NVIDIA container toolkit
 REPO_URL="https://nvidia.github.io/libnvidia-container/stable/deb/\$(ARCH)"
 GPG_KEY="/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg"
 REPO_PATH="/etc/apt/sources.list.d/nvidia-container-toolkit.list"
-
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o $GPG_KEY
 echo "deb [signed-by=$GPG_KEY] $REPO_URL /" > $REPO_PATH
-apt-get update
 
-package="nvidia-container-toolkit"
-version="latest"
-if [[ $version == "latest" ]]; then
-    apt-get install --no-install-recommends "$package"
+apt-get update -qq
+
+# cuda-toolkit vs nvidia-cuda-toolkit:
+# - cuda-toolkit is NVIDIA's official package from their repository
+# - nvidia-cuda-toolkit is Ubuntu's packaged version of CUDA toolkit (often outdated version)
+# So using cuda-toolkit here:
+apt install -y --no-install-recommends cuda-drivers cuda-toolkit nvidia-container-toolkit
+
+# Update PATH and LD_LIBRARY_PATH
+path="/usr/local/cuda-12/bin"
+library_path="/usr/local/cuda-12/lib64"
+# Ensure the paths exist
+ls -al $path
+ls -al $library_path
+prepend_etc_environment_path "$path"
+
+# prepend_etc_environment_variable does not check if the variable exists, so fails if not...
+if grep "^LD_LIBRARY_PATH=" /etc/environment; then
+    prepend_etc_environment_variable "LD_LIBRARY_PATH" "$library_path"
 else
-    version_string=$(apt-cache madison "$package" | awk '{ print $3 }' | grep "$version" | head -1)
-    apt-get install --no-install-recommends "${package}=${version_string}"
+    set_etc_environment_variable "LD_LIBRARY_PATH" "$library_path"
 fi
 
 # Configure the container runtime by using the nvidia-ctk command
