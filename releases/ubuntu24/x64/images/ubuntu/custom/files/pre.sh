eec1339c6afe0710c18fb19b17fca1e57aaf0f2b
crohr <6114+crohr@users.noreply.github.com>
2025-04-25 10:35:47 +0000

    Sync from upstream

--- releases/ubuntu24/x64/images/ubuntu/custom/files/pre.sh
+++ releases/ubuntu24/x64/images/ubuntu/custom/files/pre.sh
@@ -26,11 +26,12 @@ rm -f amazon-ssm-agent.deb
 # avoid nvme0n1: Process '/usr/bin/unshare -m /usr/bin/snap auto-import --mount=/dev/nvme0n1' failed with exit code 1.
 snap set system experimental.hotplug=false
 
-# save ~1s on cloud-init
+# saves ~1s on cloud-init (`cloud-init analyze blame`)
 arch=$(dpkg --print-architecture)
 codename=$(lsb_release --codename -s)
 sed -i 's|release = util.lsb_release()\["codename"\].*|release = "'$codename'"|w /dev/stdout' /usr/lib/python3/dist-packages/cloudinit/config/cc_apt_configure.py | grep $codename
-sed -i 's|arch = util.get_dpkg_architecture().*|arch = "'$arch'"|w /dev/stdout' /usr/lib/python3/dist-packages/cloudinit/config/cc_apt_configure.py | grep $arch
+sed -i 's|util.get_dpkg_architecture()|"'$arch'"|w /dev/stdout' /usr/lib/python3/dist-packages/cloudinit/config/cc_apt_configure.py | grep $arch
+sed -i 's|util.get_dpkg_architecture()|"'$arch'"|w /dev/stdout' /usr/lib/python3/dist-packages/cloudinit/distros/debian.py | grep $arch
 
 cat > /etc/cloud/cloud.cfg.d/01_runs_on.cfg <<EOF
 ssh_quiet_keygen: true
